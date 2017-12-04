83bd76dab54d21f4c447f07600f90e660682fb29
Larry Li <larry.zy.li@gmail.com>
2017-12-04 17:32:30 -0600

    finished problem 5 and 6 for real

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,18 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Todo from "./components/Todo.js";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userInput: "",
+      tasks: []
+    };
+
+    this.updateInput = this.updateInput.bind(this);
+    this.addTask = this.addTask.bind(this);
+  }
+  updateInput(val) {
+    this.setState({
+      userInput: val
+    });
+  }
+  addTask() {
+    const updatedArr = this.state.tasks.slice();
+    updatedArr.push(this.state.userInput);
+    this.setState({
+      tasks: updatedArr
+    });
+  }
   render() {
+    let listTasks = this.state.tasks.map((task, key) => {
+      return <Todo task={task} key={key} />;
+    });
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>My to-do list:</h1>
+        <input onChange={e => this.updateInput(e.target.value)} type="text" />
+        <button onClick={this.addTask}>Add</button>
+        {listTasks}
       </div>
     );
   }
