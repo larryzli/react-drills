c5bb6c8084629a609013d7ce522b3cf526ba3836
Larry Li <larry.zy.li@gmail.com>
2017-12-04 15:34:34 -0600

    finished prob 1

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,18 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userInput: ""
+    };
+  }
+  updateInput(val) {
+    this.setState({
+      userInput: val
+    });
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={e => this.updateInput(e.target.value)} type="text" />
+        <p>{this.state.userInput}</p>
       </div>
     );
   }
