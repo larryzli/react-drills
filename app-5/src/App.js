6b26ab8d32c102d10c73bc47a9604ffe58cfc01e
Larry Li <larry.zy.li@gmail.com>
2017-12-04 16:49:35 -0600

    finished problem 6

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Image from "./components/Image.js";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Image
+          imageSrc={
+            "https://images.pexels.com/photos/8158/night-television-tv-theme-machines.jpg?w=940&h=650&auto=compress&cs=tinysrgb"
+          }
+        />
       </div>
     );
   }
---
+++ app-5/src/components/Image.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Image = ({ imageSrc }) => <img src={imageSrc} alt="" />;
+
+export default Image;
